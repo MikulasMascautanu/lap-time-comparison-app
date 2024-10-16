4f4c17ce56ea9531725c9e3bc7bdc073e6d236e4
MikulasMascautanu <mikulas.mascautanu@profiq.com>
2024-10-16 08:20:49 +0200

    remove dotenv

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,4 @@
 import { createClient } from "@libsql/client";
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 export const dbClient = createClient({
   url: process.env.TURSO_DATABASE_URL!,
