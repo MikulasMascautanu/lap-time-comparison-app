52e1e153b9d81903d00d7b7a6d783f1116ae7d5b
MikulasMascautanu <68233869+MikulasMascautanu@users.noreply.github.com>
2025-03-11 18:24:22 +0100

    Add Australia standalone

--- src/data.ts
+++ src/data.ts
@@ -15,6 +15,7 @@ export interface Driver {
 }
 
 export const circuits: Circuit[] = [
+{ name: "Australia", country: "Australia"},
 { name: "Albert Park Circuit", country: "Australia"},
   { name: "Bahrain", country: "Bahrain" },
   { name: "Jeddah", country: "Saudi Arabia" },
